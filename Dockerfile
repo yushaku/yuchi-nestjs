ea4cc7d15e03d5e98715d1413219ad2d84cc79f5
SonLV <sonlv@nami.foundation>
2026-01-04 14:30:31 +0700

    refactor: remove unused package

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,4 @@ COPY package.json pnpm-lock.yaml ./
 RUN npm install -g pnpm
 RUN pnpm install
 COPY . .
-CMD ["./run-dev.sh"]
+CMD ["pnpm", "dev"]
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,3 @@
-import { BullModule } from '@nestjs/bullmq'
-import { CacheModule } from '@nestjs/cache-manager'
-import { redisStore } from 'cache-manager-ioredis-yet'
-
 import { Module } from '@nestjs/common'
 import { ConfigModule, ConfigService } from '@nestjs/config'
 import { TerminusModule } from '@nestjs/terminus'
@@ -22,8 +18,8 @@ import Joi from 'joi'
       isGlobal: true,
       validationSchema: Joi.object({
         DATABASE_URL: Joi.string().required(),
-        REDIS_HOST: Joi.string().required().default('localhost'),
-        REDIS_PORT: Joi.string().required().default(6379),
+        REDIS_HOST: Joi.string().required(),
+        REDIS_PORT: Joi.string().required(),
 
         JWT_SECRET: Joi.string().required(),
         COOKIE_SECRET: Joi.string().required(),
@@ -33,20 +29,6 @@ import Joi from 'joi'
 
         THROTTLE_LIMIT: Joi.number().default(20),
         THROTTLE_TTL: Joi.number().default(60),
-      }),
-    }),
-    BullModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (config: ConfigService) => ({
-        connection: {
-          host: config.get('REDIS_HOST'),
-          port: config.get('REDIS_PORT'),
-        },
-        defaultJobOptions: {
-          attempts: 3,
-          removeOnComplete: true,
-        },
       }),
     }),
     ThrottlerModule.forRootAsync({
@@ -65,18 +47,6 @@ import Joi from 'joi'
             host: config.get('REDIS_HOST'),
           }),
         ),
-      }),
-    }),
-    CacheModule.registerAsync({
-      isGlobal: true,
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (config: ConfigService) => ({
-        store: redisStore,
-        redisInstance: new Redis({
-          host: config.get('REDIS_HOST'),
-          port: config.get('REDIS_PORT'),
-        }),
       }),
     }),
     TerminusModule,
